267b4772316250e252a223e113c4b1f7390360ee
vagrant <vagrant@developer-desktop>
2017-01-12 14:08:36 +0200

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+export class Header extends Component
+{
+  render()
+  {
+    return(
+      <div className="Header">
+      <h1>Ally</h1>
+      <img src="https://www.allangray.co.za/static/images/AG-Logo.png" id="logo" alt="hello"></img>
       </div>
-    );
+    )
+  }
+}
+export class Input extends Component {
+  render()
+  {
+    return(
+        <div className="Text-field">
+          <input type="text" id="text"></input>
+          <input type="submit" id="submit"></input>
+      </div>
+    )
   }
 }
 
-export default App;
+export class Output extends Component{
+  render(){
+    return(
+        <div className="Ally-replies">
+          
+        </div>
+
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Input, Header, Output } from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <div>
+  <Header />
+  <Input />
+  <Output />
+  </div>,
+  document.getElementById('root'),
 );
