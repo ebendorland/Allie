d458e4461f563d90541e62eb20d4df022d760091
vagrant <vagrant@developer-desktop>
2017-01-12 15:27:52 +0200

    test

---
+++ src/stem.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var natural = require('natural'),
+stemmer = natural.PorterStemmer;
+// var stem = stemmer.stem('stems');
+stemmer.attach();
+
+
+
+
+
+ var stems = 'stems return'.tokenizeAndStem(true);
+
+
+
+ // console.log(stemmer);
+
+// var msg = stems
+
+var stemmer = function tokenizeAndStem(stemmer, msg) {
+
+console.log(stemmer);
+         return (msg.tokenizeAndStem(true));
+         if (msg.length == null){
+           return  console.log("What can I do for you");
+         }
+         else {
+         }
+
+}
