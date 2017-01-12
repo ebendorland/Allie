e83ec137f1c918ad1c13a19918dfc434d4ed7117
vagrant <vagrant@developer-desktop>
2017-01-12 16:24:50 +0200

    test

--- src/initialiseAlly.js
+++ src/initialiseAlly.js
@@ -4,5 +4,16 @@
   @Date: 12/01/2017
 
   @Description: This file will Initialise and setup all modules to be used in
-                the 
+                the
 */
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/../public/index.html");
+});
+
+http.listen(3000, function() {
+  console.log("Listening on localhost:3000");
+});
