0611678587c8d662b310e9e667d4239a8e6b82fc
vagrant <vagrant@developer-desktop>
2017-01-16 17:05:43 +0200

    Fixing classifier

--- src/natural/server.nlp.js
+++ src/natural/server.nlp.js
@@ -1,3 +1,31 @@
 var natural = require('natural');
-var classifier = new natural.BayesClassifier();
-var tokenizer = new natural.WordTokenizer();
+var stemmer = natural.PorterStemmer;
+var classifier = new natural.LogisticRegressionClassifier();
+stemmer.attach();
+
+/*
+  Read in the available responses the bot can give.
+*/
+var fStream = require("fs");
+var responsesJSON = fStream.readFileSync("../datasets/responses.json");
+responsesJSON = JSON.parse(responsesJSON);
+
+function processMessage(msg) {
+  var msg_stem = msg.tokenizeAndStem(true);
+  var classification = classifier.classify(msg_stem);
+
+  for (var i = 0, len = responsesJSON.length; i < len; i++) {
+    if (responsesJSON[i].type == classification) {
+      var classified_example = getClassifiedExample(trainerJSON),
+          responsesJSON[i].type);
+
+      if (classified_example != null) {
+        var similarity = getSimilarityRatio(msg_stem, classified_example);
+
+        if (similarity > 0.3) {
+          
+        }
+      }
+    }
+  }
+}
