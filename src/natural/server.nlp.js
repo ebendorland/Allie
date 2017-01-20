8af1e87ac577267de34a8a05350ddb6a34aa047e
vagrant <vagrant@developer-desktop>
2017-01-20 13:46:15 +0200

    Refactored the /src/server.nlp.js code to log every step in the process as well as fixed how a response is generated.

--- src/natural/server.nlp.js
+++ src/natural/server.nlp.js
@@ -82,21 +82,39 @@ function  getSimilarityRatio(arr1, arr2)
 
 module.exports = {
   processMessage: function(message) {
-    var data_to_ret = null;
-
-    console.log("Start of processing user input.\n Message:", message);
+    console.log("||| Start of \"processMessage()\" in",
+        "\"/src/natural/server.nlp.js\" |||");
     console.log("");
     console.log("");
 
-    Console.log("Classifying user message...\n");
+    console.log("@@@ Parameter received @@@");
+    console.log("\tParameter name:\tmessage");
+    console.log("\t\tParameter values:\t", message);
+    console.log("");
+    console.log("");
+
+    var data_response = null;
+    console.log("N.B - Variable to be used in function return",
+        "is named: \t\"data_response\"");
+    console.log("\tdata_response:\t", data_response);
+    console.log("");
+    console.log("");
+
+    console.log("Tokenizing and stemming message:\t", message.message);
+    console.log("\t\t...");
     var msg = message.message;
     var msg_stem = msg.tokenizeAndStem(true);
-    var classification = classifier.classify(msg_stem);
-    console.log("User message classified as:", classification);
+    console.log("Message tokenized and stemmed to:\t", msg_stem);
     console.log("");
     console.log("");
 
+    var classification = classifier.classify(msg_stem);
+    console.log("Message classified as:\t" + classification);
+
     // Retrieve data from server
+    console.log("Preparing classification for use in JSON server request",
+        "descriptor.");
+    console.log("\t\t...");
     var mssg = classification;
     mssg = mssg.replace("[","");
     mssg = mssg.replace("]","");
@@ -105,13 +123,23 @@ module.exports = {
     mssg = mssg.replace("'","");
     mssg = mssg.replace("'","");
     var descr = mssg.split(",");
+    console.log("JSON server request descriptor created.");
+    console.log("Descriptor:\t", descr);
+    console.log("");
+    console.log("");
 
-    console.log("Log descr[0]:", descr[0]);
-    console.log("Log descr[1]:", descr[1]);
-
+    // If descriptor is valid, query JSON server for relevant data
     if(descr[0] && descr[1])
     {
-      console.log("Requesting data from JSON server using descriptor: ", descr);
+      console.log("Descriptor is OK.");
+      console.log("");
+      console.log("");
+
+      console.log("___ Calling \"JSON.request()\" from",
+          "\"/src/natural/server.nlp.js\" ___");
+      console.log("");
+      console.log("");
+
       var sync = true;
       request({
           url:"http://localhost:3002/funds?fundReportingDescription="+descr[0],
@@ -121,47 +149,209 @@ module.exports = {
         },
         function (error,response,body,data)
         {
-          console.log("User id in httprequest:", message.id);
+          console.log("Response received from JSON server request.");
           if(error)
           {
-            console.log("Error");
-            data_to_ret = "Error: " + error;
-            console.log("This is in data_to_ret:", data_to_ret);
-            sync = false;
-          }
-          else if(classifier.length != 1)
-          {
-            console.log("This is the result from the server:", body[0][descr[1]]);
-            data_to_ret = body[0][descr[1]];
-            console.log("This is in data_to_ret:", data_to_ret);
+            console.log("\tError received:\t", error);
+            data_response = "Sorry, something is going wrong with the system" +
+                " on my end. Just give me a moment to sort this out please.";
             sync = false;
           }
           else
           {
+            console.log("\tData received:\t", body[0][descr[1]]);
+            data_response = body[0][descr[1]];
             sync = false;
           }
+          console.log("");
+          console.log("");
           //No idea what the below code is
           /*else
           {
-            data_to_ret = "Hello to you too";
+            data_response = "Hello to you too";
           }*/
-        });
-        while(sync)
+      });
+      while(sync)
+      {
+        require("deasync").sleep(100);
+      }
+    }
+    console.log("___ Returned call of \"JSON.request()\" to",
+        "\"/src/natural/server.nlp.js\" with response:\t", data_response, "___");
+    console.log("");
+    console.log("");
+
+
+    // Var to store the response to the user
+    var final_response = null;
+
+    // Loop through responses and find corresponding response
+    console.log("====")
+    console.log("Searching through responses for matching response.");
+    console.log("====")
+    console.log("");
+    for (var i = 0, len = responsesJSON.length; i < len; i++)
+    {
+      console.log("\t\tChecking response number {" + i + "} with type:\t",
+          responsesJSON[i].type);
+      console.log("");
+
+      // If a response is found with a type matching the user message's classification
+      if (responsesJSON[i].type == classification)
+      {
+        console.log("\t\tResponse found at {" + i + "} with type:\t",
+            responsesJSON[i].type);
+        console.log("");
+
+        console.log("\t\tGetting example used to train classifier for",
+            "message type of:\t", responsesJSON[i].type);
+
+        console.log("___ Calling \"getClassifiedExample()\" from",
+            "\"/src/natural/server.nlp.js\" ___");
+        console.log("");
+        console.log("");
+        var classified_example = getClassifiedExample(trainerJSON,
+            responsesJSON[i].type);
+        console.log("___ Returned call of \"getClassifiedExample()\" to",
+            "\"/src/natural/server.nlp.js\" with response:\t", classified_example, "___");
+        console.log("");
+        console.log("");
+        console.log("..");
+        console.log("\t\tTraining example:\t", classified_example);
+
+        // Check that such example does indeed exist
+        if (classified_example != null)
         {
-          require("deasync").sleep(100);
+          console.log("\t\tTraining example exists for type:\t",
+              responsesJSON[i].type);
+          console.log("");
+
+          console.log("\t\tCalculating similarity ratio between user",
+              "input and training example.");
+          console.log("\t\tMessage:\t", msg_stem);
+          console.log("\t\tTraining example:\t", classified_example);
+          console.log("");
+          console.log("");
+
+          console.log("___ Calling \"getSimilarityRatio()\" from",
+              "\"/src/natural/server.nlp.js\" ___");
+          console.log("");
+          console.log("");
+          var similarity = getSimilarityRatio(msg_stem, classified_example);
+          console.log("___ Returned call of \"getSimilarityRatio()\" to",
+              "\"/src/natural/server.nlp.js\" with response:\t", similarity, "___");
+          console.log("");
+          console.log("");
+
+          console.log("\t\tUser message and training example have",
+              "similarity ratio of: ", similarity);
+          console.log("");
+
+          // If user message and example are similar enough, copy the response
+          if (similarity > 0.3)
+          {
+            console.log("\t\tSimilarity ratio is high enough for response",
+                "to be used.");
+            console.log("");
+
+            final_response = responsesJSON[i].response;
+            console.log("\t\tUsing response: ", final_response);
+            console.log("");
+          }
         }
       }
+      console.log("~");
+      console.log("");
+    }
+    console.log("");
+    console.log("");
 
-      console.log("This is the value to be returned:", data_to_ret);
-      if(data_to_ret)
-      {
-        console.log("This is the value to be returned:", data_to_ret);
-        return (data_to_ret);
-      }
-      else
-      {
-        return ("Sorry, I don't quite get what you mean. Could you please" +
-            " be more specific?");
-      }
+    console.log("These are the values to use when compiling the response.");
+    console.log("\tJSON Data:\t", data_response);
+    console.log("\tMessage Response:\t", final_response);
+    console.log("");
+    console.log("");
+
+    // Check if there is any JSON data AND response data to return
+    if (data_response && final_response)
+    {
+      console.log("Data response exists... now compiling final response",
+          "with data:");
+      console.log("\tData response:\t", data_response);
+      console.log("\tMessage response:\t", final_response);
+      console.log("...");
+      final_response.replace("{value}", data_response);
+      console.log("Final response compiled...");
+      console.log("\tFinal Response:\t", final_response);
+      console.log("");
+      console.log("");
+    }
+    // Check if there is a response that requires data, but there is no response
+    else if (!data_response && final_response &&
+        final_response.includes("{value}"))
+    {
+      console.log("Data response does not exists... now compiling final ",
+          "response with data:");
+      console.log("\tData response:\t", data_response);
+      console.log("\tMessage response:\t", final_response);
+      console.log("...");
+      final_response = "Sorry, I was unable to find the exact " +
+          "info you're looking for. I think that's some sensitive info" +
+          " that I'm not allowed to access. Try calling the CSC maybe?";
+      console.log("Final response compiled...");
+      console.log("\tFinal Response:\t", final_response);
+      console.log("");
+      console.log("");
+    }
+    //Check if there's data but no response
+    else if (data_response && !final_response)
+    {
+      console.log("There is no response to add the data to... now compiling",
+          "a simple response.");
+      console.log("\tData response:\t", data_response);
+      console.log("\tMessage response:\t", final_response);
+      final_response = "Here you go: " + data_response +
+          "\n\nI believe that's what you're looking for?";
+      console.log("Final response compiled...");
+      console.log("\tFinal Response:\t", final_response);
+      console.log("");
+      console.log("");
+    }
+    // Check that there is a basic, value-less, response to use
+    else if (final_response)
+    {
+      console.log("There is no extra data to be added to the final",
+          "response.");
+      console.log("\tData response:\t", data_response);
+      console.log("\tMessage response:\t", final_response);
+      console.log("\tFinal Response:\t", final_response);
+      console.log("");
+      console.log("");
+    }
+    // There is no possible response. So ask for clarification
+    else
+    {
+      console.log("Unable to compile a complete response...",
+          "Compiling a simple response for clarification.");
+      console.log("\tData response:\t", data_response);
+      console.log("\tMessage response:\t", final_response);
+      final_response = "Sorry, I don't quite get what you mean. " +
+          "Could please be a bit more specific?";
+      console.log("\tFinal Response:\t", final_response);
+      console.log("");
+      console.log("");
+    }
+
+    // Return final response to server emit
+    console.log("Returning final response:\t", final_response);
+    console.log("");
+    console.log("");
+
+    console.log("||| End of \"socket.on('user_message')\" in",
+        "\"/src/socket/server.socket.js\" |||");
+    console.log("");
+    console.log("");
+
+    return (final_response);
   }
 }
--- src/socket/server.socket.js
+++ src/socket/server.socket.js
@@ -6,18 +6,48 @@ var nlp = require("../natural/server.nlp.js");
 
 //  Define user message event handler
 io.on("connection", function(socket) {
-  console.log("New user connected.");
+  console.log("+++ New user connected.");
+  console.log("\tUserID:\t", socket.id);
   console.log("");
   console.log("");
 
   socket.on("user_message", function(msg) {
-    console.log("New message from user {" + msg.id + "}: " + msg.message);
+    console.log("||| Start of \"socket.on('user_message')\" in",
+        "\"/src/socket/server.socket.js\" |||");
+    console.log("");
+    console.log("");
+
+    console.log(">>> New message from user to server.");
+    console.log("\tFrom UserID:\t", msg.id);
+    console.log("\tMessage:\t", msg.message);
+    console.log("");
+    console.log("");
+
+    console.log("___ Calling \"processMessage()\" from",
+        "\"/src/socket/server.socket.js\" ___");
     console.log("");
     console.log("");
     var response = nlp.processMessage(msg);
-    if (response)
-      io.to(msg.id).emit("server_message", response);
-    console.log("Message sent.")
+    console.log("___ Returned call of \"processMessage()\" to",
+        "\"/src/socket/server.socket.js\" with response:\t", response, "___");
+    console.log("");
+    console.log("");
+
+    console.log("<<< New message from server to user.");
+    console.log("\tTo UserID:\t", msg.id);
+    console.log("\tMessage:\t", response);
+    console.log("");
+    console.log("");
+
+    console.log("%%% Sending server response to user %%%");
+    console.log("\t...");
+    io.to(msg.id).emit("server_message", response);
+    console.log("%%% Message sent. %%%");
+    console.log("");
+    console.log("");
+
+    console.log("||| End of \"socket.on('user_message')\" in",
+        "\"/src/socket/server.socket.js\" |||");
     console.log("");
     console.log("");
   });
