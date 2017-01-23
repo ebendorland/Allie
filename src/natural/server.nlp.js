872feabe2c616d896362f69ad95c99c76f21cc47
vagrant <vagrant@developer-desktop>
2017-01-23 18:24:03 +0200

    Fix and test server.nlp.js (add responsesJSON)

--- src/natural/server.nlp.js
+++ src/natural/server.nlp.js
@@ -1,9 +1,6 @@
 var natural = require('natural');
 var fStream = require("fs");
 var request = require('request');
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
 var stemmer = natural.PorterStemmer;
 var trainClassifier = require("./ClassifierTrainer.js");
 stemmer.attach();
@@ -115,33 +112,35 @@ function getFundData(msg_stem)
 }
 
 /*
-  receive a string entered by user and classify it according to the
-  specified keywords. Return a list of keywords to use to query DB
+  Returns the correct response to use based on the parameter given
 */
-var languageProccessing = function(statement)
+function getResponse(type)
 {
-  var input = classifier.classify(statement);
-
-  input = input.replace('[','');
-  input = input.replace(']','');
-  input = (input.split("\'")).join('');
-  input = input.split(",");
-
-  console.log("Processed user message: " + input);
-  return input;
-}
-
-function  getClassifiedExample(trainerList, type) {
-  for (var i = 0, len = trainerList.length; i < len; i++) {
-    if (trainerList[i].type == type) {
-      return (trainerList[i].example.tokenizeAndStem(true));
+  for (var i = 0; i < responsesJSON.length; i++)
+  {
+    if (responsesJSON[i].type == type)
+    {
+      return (responsesJSON[i].response);
     }
   }
-  return (null);
+
+  var type_split = type.split(/(?=[A-Z])/);
+  var generic_response = "The " + type_split.join(" ") + " for your {account} "
+    + " is currently sitting at {value}. Please let me know if there's " +
+    "anything you'd like me to assist with :)";
+  return (generic_response);
 }
 
+/*
+  processMessage() takes in the user input and returns the data best fit
+  for the request, else it returns a message asking for clarification.
+*/
 module.exports = {
   processMessage: function(message) {
+
+    // Where the final response to the user will be stored
+    var final_response = "";
+
     console.log("||| Start of \"processMessage()\" in",
         "\"/src/natural/server.nlp.js\" |||");
     console.log("");
@@ -172,7 +171,19 @@ module.exports = {
     */
     // Store data retrived from JSON server here
     var data_response = null;
-    if(fund_name_classification && fund_data_classification)
+
+    /*
+      If user hasn't specified which account to use, then assume
+      that the Allan Gray account is requested. Also add a message to
+      final_response stating that the user should be more specific.
+    */
+    if (!fund_name_classification && fund_data_classification)
+    {
+      final_response = "I'm assuming you're referring to your Allan Gray " +
+        "Equity Fund? In that case here you go! ";
+      fund_name_classification = "Allan Gray Equity Fund";
+    }
+    else if(fund_name_classification && fund_data_classification)
     {
       console.log("Fund classifiers are OK.");
       console.log("");
@@ -196,8 +207,6 @@ module.exports = {
           if(error)
           {
             console.log("Error received:", error);
-            data_response = "Sorry, something is going wrong with the system" +
-                " on my end. Just give me a moment to sort this out please.";
             sync = false;
           }
           else
@@ -214,182 +223,37 @@ module.exports = {
         require("deasync").sleep(100);
       }
     }
-    console.log("___ Returned call of \"JSON.request()\" to",
-        "\"/src/natural/server.nlp.js\" with response:", data_response, "___");
-    console.log("");
-    console.log("");
-
-
-    /*// Var to store the response to the user
-    var final_response = null;
-
-    // Loop through responses and find corresponding response
-    console.log("====")
-    console.log("Searching through responses for matching response.");
-    console.log("====")
-    console.log("");
-    for (var i = 0, len = responsesJSON.length; i < len; i++)
+    // If user specified which account to draw info from but not which DATA
+    else if (fund_name_classification && !fund_data_classification)
     {
-      console.log("Checking response number {" + i + "} with type:",
-          responsesJSON[i].type);
-      console.log("");
-
-      // If a response is found with a type matching the user message's classification
-      if (responsesJSON[i].type == classification)
-      {
-        console.log("Response found at {" + i + "} with type:",
-            responsesJSON[i].type);
-        console.log("");
-
-        console.log("Getting example used to train classifier for",
-            "message type of:", responsesJSON[i].type);
-
-        console.log("___ Calling \"getClassifiedExample()\" from",
-            "\"/src/natural/server.nlp.js\" ___");
-        console.log("");
-        console.log("");
-        var classified_example = getClassifiedExample(trainerJSON,
-            responsesJSON[i].type);
-        console.log("___ Returned call of \"getClassifiedExample()\" to",
-            "\"/src/natural/server.nlp.js\" with response:", classified_example, "___");
-        console.log("");
-        console.log("");
-        console.log("..");
-        console.log("Training example:", classified_example);
-
-        // Check that such example does indeed exist
-        if (classified_example != null)
-        {
-          console.log("Training example exists for type:",
-              responsesJSON[i].type);
-          console.log("");
-
-          console.log("Calculating similarity ratio between user",
-              "input and training example.");
-          console.log("Message:", msg_stem);
-          console.log("Training example:", classified_example);
-          console.log("");
-          console.log("");
-
-          console.log("___ Calling \"getSimilarityRatio()\" from",
-              "\"/src/natural/server.nlp.js\" ___");
-          console.log("");
-          console.log("");
-          var similarity = getSimilarityRatio(msg_stem, classified_example);
-          console.log("___ Returned call of \"getSimilarityRatio()\" to",
-              "\"/src/natural/server.nlp.js\" with response:", similarity, "___");
-          console.log("");
-          console.log("");
-
-          console.log("User message and training example have",
-              "similarity ratio of: ", similarity);
-          console.log("");
-
-          // If user message and example are similar enough, copy the response
-          if (similarity > 0.3)
-          {
-            console.log("Similarity ratio is high enough for response",
-                "to be used.");
-            console.log("");
-
-            final_response = responsesJSON[i].response;
-            console.log("Using response: ", final_response);
-            console.log("");
-          }
-        }
-      }
-      console.log("~");
-      console.log("");
+      final_response = "I'm not quite sure what information you're asking" +
+        " for. Could you please also mention what you're looking for in your " +
+        fund_name_classification + "?";
     }
-    console.log("");
-    console.log("");
 
-    console.log("These are the values to use when compiling the response.");
-    console.log("JSON Data:", data_response);
-    console.log("Message Response:", final_response);
-    console.log("");
-    console.log("");
-
-    // Check if there is any JSON data AND response data to return
-    if (data_response && final_response)
+    // Search for a response if a response was provided from the JSON server
+    if (data_response)
     {
-      console.log("Data response exists... now compiling final response",
-          "with data:");
-      console.log("Data response:", data_response);
-      console.log("Message response:", final_response);
-      console.log("...");
+      final_response += getResponse(fund_data_classification);
+      final_response.replace("{account}", fund_name_classification);
       final_response.replace("{value}", data_response);
-      console.log("Final response compiled...");
-      console.log("Final Response:", final_response);
-      console.log("");
-      console.log("");
     }
-    // Check if there is a response that requires data, but there is no response
-    else if (!data_response && final_response &&
-        final_response.includes("{value}"))
-    {
-      console.log("Data response does not exists... now compiling final ",
-          "response with data:");
-      console.log("Data response:", data_response);
-      console.log("Message response:", final_response);
-      console.log("...");
-      final_response = "Sorry, I was unable to find the exact " +
-          "info you're looking for. I think that's some sensitive info" +
-          " that I'm not allowed to access. Try calling the CSC maybe?";
-      console.log("Final response compiled...");
-      console.log("Final Response:", final_response);
-      console.log("");
-      console.log("");
-    }
-    //Check if there's data but no response
-    else if (data_response && !final_response)
-    {
-      console.log("There is no response to add the data to... now compiling",
-          "a simple response.");
-      console.log("Data response:", data_response);
-      console.log("Message response:", final_response);
-      final_response = "Here you go: " + data_response +
-          "\n\nI believe that's what you're looking for?";
-      console.log("Final response compiled...");
-      console.log("Final Response:", final_response);
-      console.log("");
-      console.log("");
-    }
-    // Check that there is a basic, value-less, response to use
-    else if (final_response)
-    {
-      console.log("There is no extra data to be added to the final",
-          "response.");
-      console.log("Data response:", data_response);
-      console.log("Message response:", final_response);
-      console.log("Final Response:", final_response);
-      console.log("");
-      console.log("");
-    }
-    // There is no possible response. So ask for clarification
     else
     {
-      console.log("Unable to compile a complete response...",
-          "Compiling a simple response for clarification.");
-      console.log("Data response:", data_response);
-      console.log("Message response:", final_response);
-      final_response = "Sorry, I don't quite get what you mean. " +
-          "Could please be a bit more specific?";
-      console.log("Final Response:", final_response);
-      console.log("");
-      console.log("");
+      final_response += "Sorry, I didn't quite get what you're asking for." +
+        " Could you please tell me more or less what you need? :)";
     }
 
-    // Return final response to server emit
-    console.log("Returning final response:", final_response);
+    console.log("___ Returned call of \"JSON.request()\" to",
+        "\"/src/natural/server.nlp.js\" with response:", data_response, "___");
     console.log("");
     console.log("");
 
     console.log("||| End of \"socket.on('user_message')\" in",
         "\"/src/socket/server.socket.js\" |||");
     console.log("");
-    console.log("");*/
+    console.log("");
 
-    return (data_response);
+    return (final_response);
   }
 }
