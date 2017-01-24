aaf24cd22667d7b83d2b375bf3ae9b770de91e64
vagrant <vagrant@developer-desktop>
2017-01-24 08:50:29 +0200

    Adding Sibulele's Work

--- src/js/components/Time.js
+++ src/js/components/Time.js
@@ -5,9 +5,9 @@
 module.exports = {
   chaTime: function(){
   var d = new Date();
-  var numDay = d.getDate();
-  var numMonth = ('0' + (d.getMonth() + 1)).slice(-2);
-  var numYear = d.getFullYear();
+  // var numDay = d.getDate();
+  // var numMonth = ('0' + (d.getMonth() + 1)).slice(-2);
+  // var numYear = d.getFullYear();
   var numHours = ('0' + (d.getHours())).slice(-2);
   var numMins = ('0' + (d.getMinutes())).slice(-2);
 
