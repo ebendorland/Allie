f2dbc0ae119ce3511ff904f9c523dfb0992b7da7
vagrant <vagrant@developer-desktop>
2017-01-25 16:47:13 +0200

    Just editing UI again

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -7,30 +7,24 @@ import { Button,MenuItem,DropdownButton, ButtonToolbar,Glyphicon,Dropdown } from
 
 export default class Header extends Component{
 
-myFunction()
-{
-
-}
-
 
 render()
 {
   return(
-<div>
     <div>
 
         <div className="navigation_bar">
         </div>
 
         <div className="App-header">
-          <ul className="newnav">
-              <Link to="/"><li className="firstitem"><img src="https://www.allangray.co.za/static/images/AG-Logo.png" className="App-logo" alt="logo" /></li></Link>
-              <li><a >WHAT WE OFFER</a></li>
-              <li><a>NEWS & INSIGHTS</a></li>
-              <li><a>ABOUT US</a></li>
-              <li className="right lastitem"> <div className="invest_now"> INVEST NOW</div></li>
-              <li className="right"> <div className="log_in"> LOG IN</div></li>
-          </ul>
+            <ul className="newnav">
+                <Link to="/"><li className="firstitem"><img src="https://www.allangray.co.za/static/images/AG-Logo.png" className="App-logo" alt="logo" /></li></Link>
+                <li><a >WHAT WE OFFER</a></li>
+                <li><a>NEWS & INSIGHTS</a></li>
+                <li><a>ABOUT US</a></li>
+                <li className="right lastitem"> <div className="invest_now"> INVEST NOW</div></li>
+                <li className="right"> <div className="log_in"> LOG IN</div></li>
+            </ul>
 
             <div>
                     <ButtonToolbar>
@@ -48,16 +42,10 @@ render()
                           </Dropdown.Menu>
                         </Dropdown>
                     </ButtonToolbar>
-</div>
-
+           </div>
 
         </div>
     </div>
-
-
-
-  </div>
-
 
 )
 }}
